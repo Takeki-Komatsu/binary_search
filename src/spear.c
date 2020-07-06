bb563cef0344f6478d64496d565a4dead60fef7c
Komatsu Takeki <komatsu.t.ai@m.titech.ac.jp>
2020-07-06 19:04:38 +0900

    a

--- src/spear.c
+++ src/spear.c
@@ -6,11 +6,23 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i, lb, ub, sum;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb=1;
+  ub=100000;
+  while(ub-lb>0){
+    int mid =(ub-lb)/2;
+    sum=0;
+    for(i=0;i<n;i++){
+      sum=sum+(A[i]+mid-1)/mid;
+    }
+    if(sum>=k)ub=mid;
+    else lb=mid;
+  }
+  printf("%d\n",ub);
 
 
   return 0;
