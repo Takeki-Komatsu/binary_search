822791577f2b1ee851aed87a110e0a143a82531b
Komatsu Takeki <komatsu.t.ai@m.titech.ac.jp>
2020-07-06 18:47:41 +0900

    a

--- src/apple.c
+++ src/apple.c
@@ -6,11 +6,28 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i, lb, ub,sum;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb=0;
+  ub=n*A[n-1];
+  sum=0;
+  for(i = 0; i < n; i++){
+    sum=sum+A[i];
+  }
+  while (ub-lb>0){
+    int mid = (lb+ub)/2;
+    if((sum+n*(mid-1))/mid<=k){
+      ub=mid;
+    }
+    else{
+      lb=mid;
+    }
+  }
+  printf("%d\n",ub);
+
 
 
   return 0;
